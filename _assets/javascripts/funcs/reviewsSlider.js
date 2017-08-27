e18266355c9124c88cf9de4369495ec50cca49fa
Анатолий Зайцев <zae@teobit.ru>
2017-08-28 02:24:16 +0300

    tablet version modifications

--- _assets/javascripts/funcs/reviewsSlider.js
+++ _assets/javascripts/funcs/reviewsSlider.js
@@ -4,23 +4,23 @@ function initReviewsSlider() {
         slidesToScroll: 1,
         variableWidth: true,
         dots: true,
-        speed: 600, // BUG: this value is setted up for 1600
+        speed: 600, // BUG: this value is setted for 1600
         responsive: [
             {
                 breakpoint: 1600,
                 settings: {
                     slidesToShow: 2,
-                    speed: 800 // BUG: this value is setted up for 1366
+                    speed: 800 // BUG: this value is setted for 1366
                 }
             },
             {
                 breakpoint: 1366,
                 settings: {
-                    speed: 700 // BUG: this value is setted up as default
+                    speed: 700 // BUG: this value is setted as default
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 1023,
                 settings: {
                     mobileFirst: true,
                     arrows: false,
