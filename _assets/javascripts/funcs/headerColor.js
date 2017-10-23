6bd5c434e9946c01bb24d38f5a54efba90d79b1c
Анатолий Зайцев <zae@teobit.ru>
2017-10-23 16:53:08 +0300

    behance redesign(1366x768): fix grid, sizing and margin imprecision, add cross-browser compatibility here and there

--- _assets/javascripts/funcs/headerColor.js
+++ _assets/javascripts/funcs/headerColor.js
@@ -2,15 +2,10 @@ function headerColor() {
 
     var bodySel = 'body',
         headerSel = 'header',
-        headerHalfHeight = $(headerSel).outerHeight()/2;
+        headerHalfHeight = 0.5 * $(headerSel).outerHeight();
 
-    if ($('.page').length > 0) {
-        $(headerSel).addClass('white-bg');
-        $(headerSel).find('svg#logo').addClass('smaller');
-        $(headerSel).find('svg').removeClass('white');
-        $(headerSel).find('nav a').removeClass('white');
-    }
-    else {
+
+    function setHeaderColor() {
 
         var featuresSlideSel = '#features-slide',
             customersSlideSel = '#customers-slide',
@@ -21,41 +16,47 @@ function headerColor() {
             featuresSlideOffset = $(featuresSlideSel).offset().top,
             lastSlideOffset = $(lastSlideSel).offset().top;
 
-        $(window).on('scroll', function() {
+        if ($(bodySel).hasClass('at-desktop')) {
 
-            if ($(bodySel).hasClass('at-desktop')) {
+            if ($(window).scrollTop() < 1 || $(window).scrollTop() >= $('#slides').outerHeight() - lastSlideHeight)
+                $(headerSel).find('svg#logo').removeClass('smaller');
+            else
+                $(headerSel).find('svg#logo').addClass('smaller');
 
-                if ($(window).scrollTop() < 1 || $(window).scrollTop() == $('#slides').outerHeight() - lastSlideHeight)
-                    $(headerSel).find('svg#logo').removeClass('smaller');
-                else
-                    $(headerSel).find('svg#logo').addClass('smaller');
+        }
 
-            }
+        if (($(window).scrollTop() <= featuresSlideOffset + 0.5*featuresSlideHeight - headerHalfHeight) || ($(window).scrollTop() >= lastSlideOffset - 0.5*customersSlideHeight - headerHalfHeight)) {
 
+            if ($(bodySel).hasClass('at-mobile'))
+                $(headerSel).removeClass('white-bg');
 
-            if (($(window).scrollTop() <= featuresSlideOffset + 0.5*featuresSlideHeight - headerHalfHeight) || ($(window).scrollTop() >= lastSlideOffset - 0.5*customersSlideHeight - headerHalfHeight)) {
+            $(headerSel).find('svg, nav a')
+                .removeClass('blue')
+                .addClass('white');
 
-                if ($(bodySel).hasClass('at-mobile'))
-                    $(headerSel).removeClass('white-bg');
+        }
+        else {
 
-                $(headerSel).find('svg, nav a')
-                    .removeClass('blue')
-                    .addClass('white');
+            if ($(bodySel).hasClass('at-mobile'))
+                $(headerSel).addClass('white-bg');
 
-            }
-            else {
+            $(headerSel).find('svg, nav a')
+                .removeClass('white')
+                .addClass('blue');
 
-                if ($(bodySel).hasClass('at-mobile'))
-                    $(headerSel).addClass('white-bg');
+        }
 
-                $(headerSel).find('svg, nav a')
-                    .removeClass('white')
-                    .addClass('blue');
+    }
 
-            }
-
-        });
-
+    if ($('.page').length > 0) {
+        $(headerSel).addClass('white-bg');
+        $(headerSel).find('svg#logo').addClass('smaller');
+        $(headerSel).find('svg').removeClass('white');
+        $(headerSel).find('nav a').removeClass('white');
+    }
+    else {
+        setHeaderColor();
+        $(window).on('scroll', setHeaderColor);
     }
 
 }
